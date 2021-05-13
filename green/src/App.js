f1000112247174d43c06cfc85a0d76210182847f
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-05-13 05:55:21 +0530

    first commit

---
+++ green/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Dashboard from './Dashboard';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ green/src/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Saly from './Saly.svg';
+import DreamText from './Text.svg';
+function Dashboard() {
+    return (
+            <div>
+           
+
+        </div>
+    );
+}
+
+export default Dashboard
