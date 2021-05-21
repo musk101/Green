361632852bb0bed39d8de4ecc4b5bab2da750f00
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-05-21 19:36:21 +0530

    css changes

--- src/DesignPlant.js
+++ src/DesignPlant.js
@@ -2,13 +2,35 @@ import React from 'react'
 import plant from './Plant.svg';
 import Grid from '@material-ui/core/Grid';
 import './DesignPlant.css';
+import DesignButton from './DesignButton.svg';
+import DevelopButton from './DevelopButton.svg';
+import BusinessButton from './BusinessButton.svg';
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px",
+  }
+});
 function DesignPlant() {
+  const classes = useStyles();
     return (
         <div>
       <Grid container>
         <Grid item xs={10} sm={10} md={10} >
           <img className="plant" src={plant} alt=""/>
+        </Grid>
+        </Grid>
+        <Grid container
+        spacing={2}
+        className={classes.gridContainer}
+        justify="center">
+        <Grid item xs={7} sm={7} md={4} >
+          <img className="designbutton" src={DesignButton} alt="" height="200px" width="300px" />
+        </Grid>
+        <Grid item xs={7} sm={7} md={4} >
+          <img className="developbutton" src={DevelopButton} alt="" height="200px" width="300px"/>
         </Grid>
         </Grid>
         </div>
