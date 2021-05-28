0b76100b5880dd7d54c3d2046e0862dd6e63826b
agarwalminks <61506798+agarwalminks@users.noreply.github.com>
2021-05-29 04:27:48 +0530

    made the about us page for small screens

--- src/Aboutussmall.js
+++ src/Aboutussmall.js
@@ -5,7 +5,6 @@ import FlyingSaly from './FlyingSaly.svg';
 import aboutsecond from './aboutsecond.svg';
 import CardDesign from "./CardDesign";
 import Footer from './Footer';
-import Aboutussmall from './Aboutussmall';
 import abouttext from './abouttext.svg';
 import customercentric from './customercentric.svg';
 import integrity from './integrity.svg';
@@ -15,47 +14,47 @@ import goodteam from './goodteam.svg';
 import Grid from '@material-ui/core/Grid';
 import './Aboutus.css';
 import CardDashboardGrid from './CardDashboardGrid';
-function Aboutus() {
+function Aboutussmall() {
     return (
         <div>
-          <div className="Aboutussmall">
-          <Aboutussmall/>
-          </div>
-        <Grid container className="Aboutus">
+        
+        <Grid container>
            
-        <Grid item md={3} xs={6} sm={6}  className="rr1">
-          <img src={FlyingSaly} alt=""  width="450vw" height="auto"/>
+        <Grid item xs={3}  className="rr1">
+          <img src={FlyingSaly} alt=""  width="200vw" height="auto"/>
         </Grid>
-        <Grid item md={5} xs={6}  className="rr2">
+        <Grid item  xs={9}  className="rr2">
           <h>We, at ALPHARULE</h><br/>
 <h>We provide the wings your business needs.</h>
         </Grid>
-        <Grid item md={4} xs={6}  className="rr3">
-        <img src={aboutsecond} alt="" width="400vw" height="auto"/>
         </Grid>
-        </Grid>
-        <Grid container  justify="center" className="Aboutus">
+        <Grid container  justify="center" className="Aboutussmall">
            
-        <Grid item md={8} xs={6} sm={7} className="about_text" >
+        <Grid item xs={6} sm={7} className="about_text" >
             
-         <p>We are the destination for your online journey of ideas and innovations.</p>
+         <p className="smallpara">We are the destination for your online journey of ideas and innovations.</p>
 
-         <p>The one stop solution for all your online buisness needs. Whether you are just an individual dreaming of starting an online website or an established business creating an identity in the global market, we got you covered from 
+         <p className="smallpara">The one stop solution for all your online buisness needs. Whether you are just an individual dreaming of starting an online website or an established business creating an identity in the global market, we got you covered from 
 the initial phases of designing and developing to your branding.
  Alpharule works on the motive of ideas getting real.</p>
  
-<p>Our goal is to stand as per the client’s expectations
+<p className="smallpara">Our goal is to stand as per the client’s expectations
  and provide them the best solutions for their requirements.</p>
         </Grid>
+        <Grid item xs={6}  className="rr3">
+        <img src={aboutsecond} alt="" width="200vw" height="auto"/>
         </Grid>
-        <Grid container className="Aboutus">
+        </Grid>
+        <Grid container  justify="center" className="Aboutussmall">
            
-           <Grid item md={3} sm={5} className="whyus">
+           <Grid item xs={12} className="whyus">
                
             <p>WHY US?</p>
            </Grid>
-           <Grid item md={9}>
-           <Grid container spacing={10} justify="center" >
+           </Grid>
+           <Grid container  justify="center" >
+           <Grid item xs={6}>
+           <Grid container spacing={8} justify="center" >
            <Grid item xs={12} sm={6} md={3}   >
           <CardDesign src={integrity} alt="1"  />
         </Grid>
@@ -73,11 +72,11 @@ the initial phases of designing and developing to your branding.
               </Grid>
               </Grid>
            </Grid>
-           <div className="Aboutus">
+           {/* <div className="Aboutussmall">
            <Footer/>
-           </div>
+           </div> */}
         </div>
     )
 }
 
-export default Aboutus
+export default Aboutussmall
