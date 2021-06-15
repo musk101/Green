b63f7c246e2266f786afc03200b5be5a389dbf1a
Bablu Roy <petrospine@gmail.com>
2021-06-15 21:10:45 +0530

    develop page icons updated

--- src/LetsTalk.js
+++ src/LetsTalk.js
@@ -9,27 +9,39 @@ function LetsTalk() {
   
   return (
     <>
-<Grid container>
-<Grid item md={6} xs={12} className="text-convert text-center mt-5">
+      <Grid container>
+        <Grid item md={6} xs={12} className="text-convert text-center mt-5">
+          <Grid item md={12} xs={12}>
+            <span>Let's Talk?</span>
+          </Grid>
 
-<Grid item md={12} xs={12}>
-<span>Let's Talk?</span>
-</Grid>
-            
-<Grid item md={12} xs={12}>
-<input  label="Outlined" variant="outlined" placeholder="Name" style={{backgroundColor:"#eaf5dd"}}/>
-<input label="Outlined" variant="outlined" placeholder="E-mail" style={{backgroundColor:"#eaf5dd"}} />
-<input label="Outlined" variant="outlined" placeholder="Phone Number" style={{backgroundColor:"#eaf5dd"}}/>
-</Grid>
-         
-</Grid>
-     
-        <Grid item md={6} xs={12} >
-          <img src={contact} className="img-fluid"/>
+          <Grid item md={12} xs={12}>
+            <input
+              label="Outlined"
+              variant="outlined"
+              placeholder="Name"
+              style={{ backgroundColor: "#eaf5dd" }}
+            />
+            <input
+              label="Outlined"
+              variant="outlined"
+              placeholder="E-mail"
+              style={{ backgroundColor: "#eaf5dd" }}
+            />
+            <input
+              label="Outlined"
+              variant="outlined"
+              placeholder="Phone Number"
+              style={{ backgroundColor: "#eaf5dd" }}
+            />
+          </Grid>
         </Grid>
-       
+
+        <Grid item md={6} xs={12}>
+          <img src={contact} className="img-fluid" />
+        </Grid>
       </Grid>
-      <Footer/>
+      <Footer />
     </>
   );
 }
