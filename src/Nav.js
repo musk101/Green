69ac60a7f0763093d162a8f8d19d9468c4c3b2a8
Bablu Roy <petrospine@gmail.com>
2021-06-19 20:02:09 +0530

    bugs fixes

--- src/Nav.js
+++ src/Nav.js
@@ -55,13 +55,12 @@ const [servicesNav, SetServicesNav] = useState( false );
     <>
       <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-nav dark-button">
         <Container>
-          
           <span className="navbar-brand">
             <Link to="/">
-              <img src={Logo} alt="" className="nav-logo"/>
+              <img src={Logo} alt="" className="nav-logo" />
             </Link>
           </span>
-          
+
           <button
             className="navbar-toggler"
             data-toggle="collapse"
@@ -73,15 +72,13 @@ const [servicesNav, SetServicesNav] = useState( false );
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-      
-        
+
           <div
             className="collapse navbar-collapse bg-nav justify-content-end dark-button"
             id="navbarCollapse"
           >
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-               
                 <button
                   className="navbar-toggler toogle-btn-li"
                   data-toggle="collapse"
@@ -100,134 +97,162 @@ const [servicesNav, SetServicesNav] = useState( false );
 
               {width > 1000 ? (
                 <>
-                <li className="nav-item active">
+                  <li className="nav-item active">
                     <Link to="/">
-                      <span className="nav-link text-dark">Home</span>
+                      <span
+                        className="nav-link text-dark"
+                        onClick={servicesMenuClose}
+                      >
+                        Home
+                      </span>
                     </Link>
                   </li>
                   <li className="nav-item active">
                     <Link to="/work">
-                      <span className="nav-link text-dark">Work</span>
+                      <span className="nav-link text-dark" onClick={servicesMenuClose} 
+                      
+                      >Work</span>
                     </Link>
                   </li>
 
                   <li className="nav-item">
-                  
-                    <span className="nav-link text-dark" onClick={isServicesOpen}>
+                    <span
+                      className="nav-link text-dark"
+                      onClick={isServicesOpen}
+                    >
                       Services
-                      </span>
-                      
+                    </span>
                   </li>
 
                   <li className="nav-item">
                     <Link to="/about">
-                      <span className="nav-link text-dark">About</span>
+                      <span className="nav-link text-dark" onClick={servicesMenuClose}>About</span>
                     </Link>
                   </li>
                   <li className="nav-item">
                     <Link to="/">
-                  <img src={nightday} className="dark-button" style={{paddingRight: "2.7rem",paddingTop: "0.3rem"}}/>
-                  </Link>
+                      <img
+                        src={nightday}
+                        className="dark-button"
+                        style={{ paddingRight: "2.7rem", paddingTop: "0.3rem" }}
+                      />
+                    </Link>
                   </li>
                   <li>
                     <Link to="/letstalk">
                       <span
                         className="nav-link text-center text-uppercase"
                         id="lets_talk"
+                        onClick={servicesMenuClose}
                       >
                         Let's Talk
                       </span>
                     </Link>
                   </li>
                 </>
-              )  : (
+              ) : (
                 <>
                   <span className="mt-5"></span>
                   <h2 className="services-text mx-4">SERVICES</h2>
                   <li className="nav-item active">
                     <span className="nav-link">
                       <Link to="/design">
-                  <img src={DesignButton} className="img-fluid"
-                  data-toggle="collapse"
-                  data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={removeBackgroundBlur}
-                  />
+                        <img
+                          src={DesignButton}
+                          className="img-fluid"
+                          data-toggle="collapse"
+                          data-target="#navbarCollapse"
+                          aria-controls="navbarCollapse"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                          onClick={removeBackgroundBlur}
+                        />
                       </Link>
                     </span>
                   </li>
                   <li className="nav-item">
-                      <Link to="/develop">
-                        <span className="nav-link">
-                          <img src={DevelopButton} className="img-fluid"
-                  data-toggle="collapse"
-                  data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={removeBackgroundBlur}
-                      />
-                    </span></Link>
+                    <Link to="/develop">
+                      <span className="nav-link">
+                        <img
+                          src={DevelopButton}
+                          className="img-fluid"
+                          data-toggle="collapse"
+                          data-target="#navbarCollapse"
+                          aria-controls="navbarCollapse"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                          onClick={removeBackgroundBlur}
+                        />
+                      </span>
+                    </Link>
                   </li>
-                    <li className="nav-item">
+                  <li className="nav-item">
                     <Link to="/market">
-                    <span className="nav-link">
-                          <img src={BusinessButton} className="img-fluid"
-                  data-toggle="collapse"
-                  data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={removeBackgroundBlur}
-                      />
-                        </span>
-                        </Link>
+                      <span className="nav-link">
+                        <img
+                          src={BusinessButton}
+                          className="img-fluid"
+                          data-toggle="collapse"
+                          data-target="#navbarCollapse"
+                          aria-controls="navbarCollapse"
+                          aria-expanded="false"
+                          aria-label="Toggle navigation"
+                          onClick={removeBackgroundBlur}
+                        />
+                      </span>
+                    </Link>
                   </li>
                   <Link to="/work">
                     <li className="nav-item  nav-sidebar-text">
-                        <span className="mx-4"
+                      <span
+                        className="mx-4"
                         data-toggle="collapse"
-                  data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={removeBackgroundBlur}
-                        >
+                        data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={removeBackgroundBlur}
+                      >
                         WORK
                       </span>
                     </li>
                   </Link>
                   <Link to="/about">
                     <li className="nav-item nav-sidebar-text mt-4">
-                        <span className="mx-4"
+                      <span
+                        className="mx-4"
                         data-toggle="collapse"
-                  data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={removeBackgroundBlur}
-                        >
+                        data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={removeBackgroundBlur}
+                      >
                         ABOUT
                       </span>
                     </li>
                   </Link>
 
                   <li className="lets-talk-sidebar-div mt-4">
-                    <span className="side-nav-email"><a href="mailto:contact@alpharule.in">contact@alpharule.in</a></span>
-                      <Link to="/letstalk"><span
+                    <span className="side-nav-email">
+                      <a href="mailto:contact@alpharule.in">
+                        contact@alpharule.in
+                      </a>
+                    </span>
+                    <Link to="/letstalk">
+                      <span
                         className="nav-link text-center text-uppercase"
                         id="lets_talk"
                         data-toggle="collapse"
-                  data-target="#navbarCollapse"
-                  aria-controls="navbarCollapse"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                  onClick={removeBackgroundBlur}
+                        data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={removeBackgroundBlur}
                       >
                         Let's Talk
-                      </span></Link>
+                      </span>
+                    </Link>
                   </li>
                 </>
               )}
@@ -244,8 +269,11 @@ const [servicesNav, SetServicesNav] = useState( false );
       {/* services menu on desktop */}
       {servicesNav === true ? (
         <div className="nav-serices-menu text-center">
-          <Grid container className="reverse-items nav-services" justify="center">
-            
+          <Grid
+            container
+            className="reverse-items nav-services"
+            justify="center"
+          >
             <Grid item sm={6} md={2}>
               <Link to="/design">
                 <img
